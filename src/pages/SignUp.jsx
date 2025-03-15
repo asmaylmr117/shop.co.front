8b8ae8729be8d9bf57dcbc58feabb25b0d7bffc4
Mostafa Ismail Alanani <asmaylmr117@gmail.com>
2025-03-15 02:50:57 +0200

    SignUp.jsx

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -18,7 +18,7 @@ export default function Signup() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3000/api/auth/signup', {
+      const response = await fetch('https://shop-co-back.vercel.app/api/auth/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
